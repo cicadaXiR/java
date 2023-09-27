60035f4948a2f27bf237364f8b24cc867182e338
cicadaXiR <89849656+cicadaXiR@users.noreply.github.com>
2023-09-27 17:41:31 +0530

    Modifications in BinaryTree.java

--- com/datastructureAndalgorithms/BinaryTree.java
+++ com/datastructureAndalgorithms/BinaryTree.java
@@ -1,36 +1,41 @@
 package com.datastructureAndalgorithms;
 
 class Node1 {
-    int data;
-    Node1 right;
-    Node1 left;
-
-    public Node1(int data) {
-        this.data = data;
-        this.right = null;
-        this.left = null;
+    public int data;
+    public Node1 rightNode;
+    public Node1 leftNode;
+    public Node1(){
+    }
+    public void result(){
+        System.out.println("(" + data +")");
     }
 }
 class Btree{
-        int index = -1;
-        public Node1 buildTree(int node[]){
-            index++;
-            if(node[index] == -1){
-                return null;
-            }
-            Node1 newNode = new Node1(node[index]);
-            newNode.right = buildTree(node);
-            newNode.left = buildTree(node);
-            return newNode;
-        }
+    private Node1 root;
+    public Btree(){
+        root = null;
     }
-
+    public Node1 search(int data){
+        Node1 current = root;
+        System.out.println("Visiting elements:");
+        while(current.data != data){
+            if(current != null){
+                System.out.println(current.data+ " ");
+                if(current.data > data){
+                    current = current.leftNode;
+                }else{
+                    current = current.rightNode
+                }
+                if(current == null){
+                    return null;
+                }
+            }
+        }
+        return current;
+    }
+}
 public class BinaryTree {
     public static void main(String[] args) {
-        int node[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
 
-        Btree tree = new Btree();
-        Node1 root = tree.buildTree(node);
-        System.out.println(root.data);
     }
 }
